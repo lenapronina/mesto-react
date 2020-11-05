857912093ec2cf3c1f32c9c959c29aadc958a740
Lena Pronina <kaknetak@gmail.com>
2020-11-06 00:26:23 +0300

    feat: update favicon, add loader

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,9 @@ import React from 'react';
 import api from '../utils/api';
 import Card from './Card';
 
+import Loader from 'react-loader-spinner';
+
+
 function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}){
 
   const [userAvatar, setUserAvatar] = React.useState('');
@@ -9,10 +12,16 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}){
   const [userDescription, setUserDescription] = React.useState('');
   const [cards, setCards] = React.useState([]);
 
+  const[isLoading, setIsLoading] =React.useState(false);
+
   React.useEffect(() => {
+    // run Loader
+    setIsLoading(true);
+    //fetch profile and cards data
     api.getAllInitialData()
       .then((data) => {
         const [ initialCardsList, userProfileData] = data;
+        //update data with server response
         setUserAvatar(userProfileData.avatar);
         setUserName(userProfileData.name);
         setUserDescription(userProfileData.about);
@@ -22,7 +31,9 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}){
           name: card.name,
           link: card.link,
           likes: card.likes
-        })))
+        })));
+        // stop Loader
+        setIsLoading(false);
       })
       .catch((err)=> console.log(err));
   }, []);
@@ -30,17 +41,19 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}){
   return(
     <main className="main page__main">
       <section className="profile">
-        <div className="profile__avatar" onClick={onEditAvatar} style={{ backgroundImage: `url(${userAvatar})` }} ></div>
+        <div className="profile__avatar" onClick={onEditAvatar} style={isLoading?{backgroundColor: `#cccccc`}:{ backgroundImage: `url(${userAvatar})` }} ></div>
         <div className="profile__info">
-          <h1  className="profile__title">{userName}</h1>
+          <h1  className="profile__title">{isLoading? 'User' : userName}</h1>
           <button className="profile__edit-button" onClick={onEditProfile}></button>
-          <p className="profile__subtitle">{userDescription}</p>
+          <p className="profile__subtitle">{isLoading? 'Description': userDescription}</p>
         </div>
         <button className="profile__add-button" onClick={onAddPlace}></button>
       </section>
       <section className="mesto-collection main__mesto-collection">
         <ul className="mesto-cards">
-          { cards.map((card)=> <Card card={card} key={card.id} onCardClick={onCardClick}/>) }
+          { isLoading
+            ? <Loader type="ThreeDots" color="#ffffff" height={60} width={60} style={{ margin: 'auto'}} />
+            : cards.map((card)=> <Card card={card} key={card.id} onCardClick={onCardClick}/>) }
         </ul>
       </section>
     </main>
